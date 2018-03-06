fc15d0d94d07f41bd7b3ebc5f25366009b3cdbef
Rodrigo de Lucena <rodrigo.lucena83@outlook.com>
2018-03-06 20:51:32 -0300

    Retomando o projeto

---
+++ src/br/com/nutellatux/remedyWeb/dao/DeptoDao.java
@@ -0,0 +1,37 @@
+package br.com.nutellatux.remedyWeb.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.nutellatux.remedyWeb.modelo.Depto;
+
+@Stateless
+public class DeptoDao {  //TODO Como usar o Datasource no TomCat se ele é só um containner????
+						// C3PO para aplicações que não tem DS.
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public void adiciona (Depto depto) {
+		this.manager.persist(depto);
+	}
+	
+	public Depto busca (Integer id) {
+		return this.manager.find(Depto.class, id);
+	}
+	
+	public List<Depto> lista(){
+		return this.manager.createQuery("Select d From Depto d").getResultList();
+	}
+	
+	public void remove (Depto depto) {
+		this.manager.remove(depto);
+	}
+	
+	
+	
+	
+}
---
+++ src/br/com/nutellatux/remedyWeb/mb/DeptoBean.java
@@ -0,0 +1,67 @@
+package br.com.nutellatux.remedyWeb.mb;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.nutellatux.remedyWeb.dao.DeptoDao;
+import br.com.nutellatux.remedyWeb.modelo.Depto;
+
+@Named
+@ViewScoped
+public class DeptoBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private DeptoDao deptoDao;
+
+	private Depto depto = new Depto();
+	private List<Depto> deptos; // Tem que ser Private?????
+
+	public void grava() {
+		this.deptoDao.adiciona(depto);
+		this.deptos = deptoDao.lista();
+
+		limpaFormularioDoJSF();
+	}
+
+	public void remove (Integer id) {
+		this.depto = this.deptoDao.busca(id);
+		this.deptoDao.remove(this.depto);
+		
+		this.deptos = deptoDao.lista();
+		
+		limpaFormularioDoJSF();
+	}
+	
+	public void limpaFormularioDoJSF() {
+		this.depto = new Depto();
+	}
+	
+	
+	
+	
+	// Getters e Setters
+
+	public Depto getDepto() {
+		return depto;
+	}
+
+	public void setDepto(Depto depto) {
+		this.depto = depto;
+	}
+
+	public List<Depto> getDeptos() {
+		if (this.deptos == null) {
+			this.deptos = deptoDao.lista();
+		}
+		return deptos;
+	}
+}
